6cf49c9040d381d0d8d8c1bfac727a8e0b3a1973
Akshit Mehdiratta <ameh0025@student.monash.edu>
2023-10-14 04:59:58 +1100

    inserted 5 emergency contacts

--- Assignments/Ass2A/T2-mns-insert.sql
+++ Assignments/Ass2A/T2-mns-insert.sql
@@ -16,7 +16,12 @@
 --------------------------------------
 --INSERT INTO emergency_contact
 --------------------------------------
-INSERT INTO emergency_contact (ec_id, ec_fname, ec_lname, ec_phone)
+INSERT INTO emergency_contact (ec_id, ec_fname, ec_lname, ec_phone) VALUES (1, 'Tim', 'Zo', '6416666666');
+INSERT INTO emergency_contact (ec_id, ec_fname, ec_lname, ec_phone) VALUES (2, 'Tam', 'Xo', '6417777777');
+INSERT INTO emergency_contact (ec_id, ec_fname, ec_lname, ec_phone) VALUES (3, 'Jim', 'Qo', '6418888888');
+INSERT INTO emergency_contact (ec_id, ec_fname, ec_lname, ec_phone) VALUES (4, 'Jam', 'Wo', '6419999999');
+INSERT INTO emergency_contact (ec_id, ec_fname, ec_lname, ec_phone) VALUES (5, 'Sam', 'Mo', '6410000000');
+
 
 --------------------------------------
 --INSERT INTO patient
