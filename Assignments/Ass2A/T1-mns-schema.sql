007e32e720a0ab125ac8fe2533bad7cdc2b4addb
Akshit Mehdiratta <ameh0025@student.monash.edu>
2023-10-14 02:21:46 +1100

    created table emergency_contact

--- Assignments/Ass2A/T1-mns-schema.sql
+++ Assignments/Ass2A/T1-mns-schema.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T1-mns-schema.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 34165479
+--Student Name: Akshit Mehdiratta
+--Unit Code: FIT9132
+--Assignment 2a
 
 /* Comments for your marker:
 
@@ -21,9 +21,16 @@
 
 
 
+
 -- TABLE: EMERGENCY_CONTACT
 
-
+Create Table emergency_contact (
+    ec_id    CHAR(10) NOT NULL,
+    ec_fname VARCHAR2(20) NOT NULL,
+    ec_lname VARCHAR2(20) NOT NULL,
+    ec_phone CHAR(12) NOT NULL,
+    CONSTRAINT emergency_contact_pk PRIMARY KEY ( ec_id ),
+    CONSTRAINT emergency_contact_uq UNIQUE ( ec_phone ));
 
 -- TABLE: PATIENT
 
