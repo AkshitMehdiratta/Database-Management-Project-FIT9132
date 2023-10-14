9e76254e7195e812dcb323eb230c47fbf0827a78
Akshit Mehdiratta <ameh0025@student.monash.edu>
2023-10-14 16:03:53 +1100

    start 4a

--- Assignments/Ass2A/T4-mns-alter.sql
+++ Assignments/Ass2A/T4-mns-alter.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T4-mns-alter.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 34165479
+--Student Name: Akshit Mehdiratta
+--Unit Code: FIT9132
+--Applied Class No: 04_OnCampus
 
 /* Comments for your marker:
 
@@ -14,7 +14,8 @@
 */
 
 --4(a)
-
+ALTER TABLE training_course ADD (
+     traincourse_type CHAR(1) DEFAULT 'P' NOT NULL,
 
 --4(b)
 
