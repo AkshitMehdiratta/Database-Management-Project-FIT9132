3fd7ff003e85a335eb81bf6062c35f7738c6f0db
Akshit Mehdiratta <ameh0025@student.monash.edu>
2023-10-14 13:00:44 +1100

    3a complete created seq

--- Assignments/Ass2A/T3-mns-dm.sql
+++ Assignments/Ass2A/T3-mns-dm.sql
@@ -10,23 +10,29 @@
 
 
 
-
 */
 
-
 --3(a)
-DROP
-CREATE SEQUENCE manuf_seq
-         START WITH 100 INCREMENT BY 1;
+
+-- for emergency contact
+DROP SEQUENCE emergency_contact_seq;
+
+CREATE SEQUENCE emergency_contact_seq START WITH 100 INCREMENT BY 5;
+
+-- for patient
+DROP SEQUENCE patient_seq;
+
+CREATE SEQUENCE patient_seq START WITH 100 INCREMENT BY 5;
+
+-- for appointment
+DROP SEQUENCE appointment_seq;
+
+CREATE SEQUENCE appointment_seq START WITH 100 INCREMENT BY 5;
 
 --3(b)
 
-
 --3(c)
-
 
 --3(d)
 
-
 --3(e)
-
