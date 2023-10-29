869a18796827f5406f8ed2f525b6e27a28cea176
Akshit Mehdiratta <ameh0025@student.monash.edu>
2023-10-29 18:07:58 +1100

    Task 1 2a

--- Assignments/Ass2B/T2-mns-select.sql
+++ Assignments/Ass2B/T2-mns-select.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T2-mns-select.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 34165479
+--Student Name: Akshit Mehdiratta
+--Unit Code: FIT9132
+--Applied Class No: 04_OnCampus
 
 /* Comments for your marker:
 
@@ -17,6 +17,9 @@
 -- PLEASE PLACE REQUIRED SQL SELECT STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
+
+Select item_id, item_desc, item_stdcost, item_stock from mns.item where item_stock>50 and item_desc LIKE 'composite%'
+order by item_stock DESC, id;
 
 
 
