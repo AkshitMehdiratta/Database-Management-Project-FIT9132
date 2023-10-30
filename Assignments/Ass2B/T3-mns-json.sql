152653fe7f5a8b369dc97e8706e443ab43ce2d7d
Akshit Mehdiratta <ameh0025@student.monash.edu>
2023-10-30 19:01:08 +1100

    Task 3a checkpoint

--- Assignments/Ass2B/T3-mns-json.sql
+++ Assignments/Ass2B/T3-mns-json.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T3-mns-json.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 34165479
+--Student Name: Akshit Mehdiratta
+--Unit Code: FIT9132
+--Applied Class No: 04_OnCampus
 
 /* Comments for your marker:
 
@@ -18,4 +18,21 @@
 -- THE COLLECTION OF JSON DOCUMENTS HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
+
+SET PAGESIZE 300
+
+SELECT
+    JSON_OBJECT ('_id' VALUE a.appt_no,
+                 'datetime' VALUE TO_CHAR(a.appt_datetime, 'DD/MM/YYYY HH24:MI'),
+                 'provider_code' VALUE p.provider_code,
+                 'dob' VALUE to_char(studob, 'dd-mm-yyyy'),
+                 'enrolmentInfo' VALUE JSON_ARRAYAGG(
+                                JSON_OBJECT('unitcode' VALUE unitcode,
+                                'unitname' VALUE unitname,
+                                'year' VALUE to_char(ofyear, 'yyyy'),
+                                'semester' VALUE ofsemester,
+                                'mark' VALUE enrolmark,
+                                'grade' VALUE enrolgrade))
+    FORMAT JSON )
+    || ','
 
