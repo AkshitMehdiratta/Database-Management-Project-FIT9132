b3be10bd38b04f8b9136ad9f2ad7b72808bffa70
Akshit Mehdiratta <ameh0025@student.monash.edu>
2023-10-30 19:18:18 +1100

    task 3 a formatting left

--- Assignments/Ass2B/T3-mns-json.sql
+++ Assignments/Ass2B/T3-mns-json.sql
@@ -22,17 +22,47 @@
 SET PAGESIZE 300
 
 SELECT
-    JSON_OBJECT ('_id' VALUE a.appt_no,
-                 'datetime' VALUE TO_CHAR(a.appt_datetime, 'DD/MM/YYYY HH24:MI'),
-                 'provider_code' VALUE p.provider_code,
-                 'dob' VALUE to_char(studob, 'dd-mm-yyyy'),
-                 'enrolmentInfo' VALUE JSON_ARRAYAGG(
-                                JSON_OBJECT('unitcode' VALUE unitcode,
-                                'unitname' VALUE unitname,
-                                'year' VALUE to_char(ofyear, 'yyyy'),
-                                'semester' VALUE ofsemester,
-                                'mark' VALUE enrolmark,
-                                'grade' VALUE enrolgrade))
-    FORMAT JSON )
-    || ','
-
+        JSON_OBJECT(
+            '_id' VALUE a.appt_no,
+                    'datetime' VALUE to_char(a.appt_datetime, 'DD/MM/YYYY HH24:MI'),
+                    'provider_code' VALUE p.provider_code,
+                    'provider_name' VALUE p.provider_title
+                                          || '. '
+                                          || p.provider_fname
+                                          || ' '
+                                          || p.provider_lname,
+                    'item_totalcost' VALUE nvl(SUM(i.item_stdcost * ai.as_item_quantity
+                    ),
+                                               0),
+                    'no_of_items' VALUE COUNT(i.item_id),
+                    'items' VALUE JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'id' VALUE i.item_id,
+                    'desc' VALUE i.item_desc,
+                    'standardcost' VALUE i.item_stdcost,
+                    'quantity' VALUE i.item_stock
+                )
+            )
+        FORMAT JSON)
+        || ','
+FROM
+         mns.appointment a
+    JOIN mns.apptservice_item ai
+    ON a.appt_no = ai.as_id
+    JOIN mns.provider         p
+    ON a.provider_code = p.provider_code
+    JOIN mns.item             i
+    ON ai.item_id = i.item_id
+GROUP BY
+    a.appt_no,
+    a.appt_datetime,
+    p.provider_code,
+    p.provider_title,
+    p.provider_fname,
+    p.provider_lname,
+    i.item_id,
+    i.item_desc,
+    i.item_stdcost,
+    i.item_stock
+ORDER BY
+    a.appt_no;
